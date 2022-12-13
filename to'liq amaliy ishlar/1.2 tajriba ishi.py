feb8e47618e42b67ab356715ac32728e80174666
XoshimjonovaXurshida <116579513+XoshimjonovaXurshida@users.noreply.github.com>
2022-12-13 22:52:59 +0500

    Add files via upload

---
+++ to'liq amaliy ishlar/1.2 tajriba ishi.py
@@ -0,0 +1,8 @@
+from cmath import sqrt
+
+
+import math
+g=10
+h=int(input("balandlikni kiriting h= "))
+t=sqrt((2*h)/g)
+print("tushish vaqti",t)
---
+++ to'liq amaliy ishlar/3.2 tajriba.py
@@ -0,0 +1,7 @@
+s=input("1-satrni kiriting\t")
+a=input("2-satrni kiriting\t")
+b=input("belgini kiriting\t")
+for i in s:
+    if i==b:
+        break
+print(s,a)
